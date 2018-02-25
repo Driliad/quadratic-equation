5fc3f083a74e2a07c5d4a255c30d9e2cf49d9139
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2018-02-25 16:20:14 +0300

    100prc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var regexp =  /\-?\s?\d+/g;
+  var getValues = equation.match(regexp);
+  getValues.splice(1, 1);
+
+  getValues.forEach(function(elemnt, index) {
+    getValues[index] = elemnt.replace(/\s+/g, '');
+    getValues[index] = Number(getValues[index]);
+  });
+
+  var result = [];
+
+  // console.log(getValues);
+  var discr = discriminant(getValues);
+  // console.log(discr);
+  // console.log(getValues[1], discr, getValues[0]);
+
+  result.push(Math.round((-(getValues[1]) + discr) / (2 * getValues[0])));
+  result.push(Math.round((-(getValues[1]) - discr) / (2 * getValues[0])));
+
+  result.sort((a, b) =>  a - b);
+
+  return result;
 }
+
+var discriminant = (values = []) => Math.sqrt(values[1]**2 - 4 * values[0] * values[2]);
