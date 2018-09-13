9b82636a0fc8c4c82a8a2c717f5d06609eb74345
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2018-09-13 18:40:45 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-  var regexp = /\-?\s?\d+/g;
-  var coefficients = equation.match(regexp);
+	const regexp = /\-?\s?\d+/g;
+	const coefficients = equation.match(regexp);
 
-  coefficients.splice(1, 1);
+	coefficients.splice(1, 1);
 
-  coefficients.forEach(function (currentElement, index) {
-    coefficients[index] = currentElement.replace(/\s+/g, ''); +
-    currentElement;
-  });
+	coefficients.forEach((currentElement, index) => {
+		coefficients[index] = currentElement.replace(/\s+/g, '');
+		+currentElement;
+	});
 
-  var disc = discriminant(coefficients);
+	const disc = discriminant(coefficients);
 
-  if (disc < 0) return {
-    Error: 'Discriminant less than zero'
-  }
-  if (disc === 0) return -(coefficients[1] / (2 * coefficients[0]));
+	if (disc < 0)
+		return {
+			Error: 'Discriminant less than zero',
+		};
+	if (disc === 0) return -(coefficients[1] / (2 * coefficients[0]));
 
-  var result = [];
+	const result = [];
 
-  result.push(Math.round((-(coefficients[1]) + disc) / (2 * coefficients[0])));
-  result.push(Math.round((-(coefficients[1]) - disc) / (2 * coefficients[0])));
+	result.push(Math.round((-coefficients[1] + disc) / (2 * coefficients[0])));
+	result.push(Math.round((-coefficients[1] - disc) / (2 * coefficients[0])));
 
-  result.sort((a, b) => a - b);
+	result.sort((a, b) => a - b);
 
-  return result;
-}
+	return result;
+};
 
-var discriminant = (values = []) => Math.sqrt(values[1] ** 2 - 4 * values[0] * values[2]);
+var discriminant = (values = []) =>
+	Math.sqrt(values[1] ** 2 - 4 * values[0] * values[2]);
